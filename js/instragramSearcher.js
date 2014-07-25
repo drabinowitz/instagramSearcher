316b2e5ec476538ea0b13885401081d0c37ec080
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-25 09:24:21 -0400

    completed http call

--- js/instragramSearcher.js
+++ js/instragramSearcher.js
@@ -1,8 +1,12 @@
-angular.module('instagramSearcher',[]).
+angular.module('instagramSearcher',['ngAnimate']).
 	
-	controller('instaSearchCtrl',function($scope,$timeout,$q,$http){
+	controller('instaSearchCtrl',function($rootScope,$scope,$timeout,$q,$http){
 
 		$scope.submitted = false;
+
+		$scope.gotResults = false;
+
+		$scope.error = false;
 
 		function wait() {
 
@@ -18,28 +22,86 @@ angular.module('instagramSearcher',[]).
 
 		};
 
+		function notify(onThis) {
+
+			wait().
+
+			then(function(){
+
+				$scope[onThis] = false;
+
+			});
+
+		};
+
 		$scope.instagramSearch = function(keyword){
 
 			$scope.submitted = true;
 
 			if($scope.instaForm.$valid){
 
-				wait().then(function(){
+				var url = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent";
+
+				var request = {
+
+					callback: 'JSON_CALLBACK',
+
+					client_id: 'ba2df9d0085a4c5980f0a9c1d24b7d29'
+
+				};
+
+				$http({
+
+					method: 'JSONP',
+
+					url:url,
+
+					params:request
+
+				}).
+
+				success(function(results){
 
 					$scope.submitted = false;
 
-				})
+					$rootScope.$broadcast('instaResultsReturned',keyword,results);
+
+					console.log(results);
+
+				}).
+
+				error(function(){
+
+					$scope.submitted = false;
+
+					notify('error');
+
+				});
 
 			} else {
 
-				wait().then(function(){
-
-					$scope.submitted = false;
-
-				});
+				notify('submitted');
 
 			}
 
 		};
+
+	})
+
+	.controller('instaResultsCtrl',function($scope,$q){
+
+		$scope.results = [];
+
+		$scope.keyword = undefined;
+
+		$scope.$on('instaResultsReturned',function(event,keyword,results){
+
+			console.log('confirmed');
+
+			$scope.keyword = keyword;
+
+			$scope.results = results.data;
+
+		})
 
 	});
